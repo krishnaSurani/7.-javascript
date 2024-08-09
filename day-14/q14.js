6aa098c382ab98b6abf9bfc07296f9bf41a5f2ed
KrishnaSurani <krishnasurani07@gmail.com>
2024-08-09 22:23:08 +0530

    day-14

---
+++ day-14/q14.js
@@ -0,0 +1,21 @@
+let i;
+let j;
+
+for(i=1; i<=5; i++)
+    {
+        let a="";
+        for(j=i; j>=1; j--)
+            {
+                a +=j+" ";
+            }
+        console.log(a);
+    }
+    for(i=4; i>=1; i--)
+        {
+            let a="";
+            for(j=i; j>=1; j--)
+                {
+                    a +=j+" ";
+                }
+            console.log(a);
+        }
