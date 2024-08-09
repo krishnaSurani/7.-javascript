de21c3b85304c280d8ce0c1f343ea254a563a48c
KrishnaSurani <krishnasurani07@gmail.com>
2024-08-09 22:03:51 +0530

    day-1

---
+++ day-1/script.js
@@ -0,0 +1,52 @@
+
+document.getElementById("profile").innerHTML=
+`  
+    <div style="height:185px;width:50%; margin-left:25%;margin-top:5%; border-radius: 6px;" id="photo"><img src="image/img.jpg" alt=""></div>
+    <h1 style="text-align: center;">Krishna <br>surani</h1>
+
+    <hr style="height:2px;width:60px; background-color:#FFCD4B;text-align:center;border:none;">
+
+    <div style=" height:40px; width:100% ;background-color: rgb(248, 248, 248);    border-bottom-left-radius: 10px;
+        justify-content: center;
+        border-bottom-right-radius: 10px;margin-top:28px;display:flex;align-items:center;"><i class="fa-brands fa-instagram" style="font-size:20px;"></i><i class="fa-brands fa-linkedin" style="margin-left:20px; font-size:20px;"></i><i class="fa-brands fa-github" style="margin-left:20px; font-size:20px;"></i></div>
+`
+
+document.getElementById("detail").innerHTML=
+`
+    <div style="height:40px;width:70%;margin-left:15%; background-color: #baedfa;margin-top:50px ;display:flex;align-items:center;">
+    <h2 style="margin-left:20px;">PROFILE</h2>
+    </div>
+    <p style="margin-left:15%; font-size:15px;margin-top:20px;">IT'S STUDENT</p>
+    <p style="margin-left:15%; font-size:15px;width:58%;margin-bottom:30px;">I'M THE STUDENT AT RED AND WHITE MULTIMEDIA EDUCATION PURSUING A    <b>FULLSTACK</b> DEVELOPMENT COURSE</p>
+
+    <div style="height:40px;width:70%;margin-left:15%; background-color: #baedfa;margin-top:50px ;display:flex;align-items:center;">
+    <h2 style="margin-left:20px;">SKILLS</h2>
+    </div>
+    <ul style="margin-left:12%;margin-top:20px; font-size:15px;line-height:28px; margin-bottom:30px;">
+    <li>HTML-5</li>
+    <li>CSS-3</li>   
+    <li>MEDIA-QUERY</li>
+    <li>J-QUERY</li>   
+    <li>BOOTSTRAP</li>   
+    <li>C-LANGUAGE</li>   
+    <li>PHOTOSHOP(UI/UX)</li>
+    </ul>
+
+
+    <div style="height:40px;width:70%;margin-left:15%; background-color: #baedfa;margin-top:50px ;display:flex;align-items:center;">
+    <h2 style="margin-left:20px;">CONTACT</h2>
+    </div>
+    <p style="margin-left:16%; font-size:15px;margin-top:20px;"><i class="fa-solid fa-phone"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+91 9974477226</p>
+    <p style="margin-left:16%; font-size:15px;margin-top:20px;"><i class="fa-solid fa-envelope"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;krishnasurani07@gmail.com</p>
+    <p style="margin-left:16%; font-size:15px;margin-top:20px; margin-bottom:30px;"><i class="fa-solid fa-location-dot"></i></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rajkot-360003 ,Gujrat</p>
+
+
+    <div style="height:40px;width:70%;margin-left:15%; background-color: #baedfa;margin-top:50px ;display:flex;align-items:center;">
+    <h2 style="margin-left:20px;">EDUCATION</h2>
+    </div>
+    <p style="margin-left:16%; font-size:15px;margin-top:20px;font-weight:bold;">GUJRAT TECHNOLOGICAL UNIVERSITY</p>
+    <p style="margin-left:16%; font-size:15px;margin-top:10px;">DIPLOMA IN COMPUTER ENGINEERING</p>
+    <p style="margin-left:16%; font-size:15px;margin-top:10px; margin-bottom:30px;">2023-2028</p>
+    
+
+`
