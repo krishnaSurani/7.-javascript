3222ee666f618bbd668e7c6a37c6883ba01e7d9b
KrishnaSurani <krishnasurani07@gmail.com>
2024-08-09 22:08:03 +0530

    day-6

---
+++ day-6/q1.js
@@ -0,0 +1,39 @@
+// Question 1: Determine the minimum and maximum of three given numbers.
+
+// Input: 10, 15, 5
+// Output: Minimum: 5, Maximum: 15
+
+let a=10;
+let b=15;
+let c=5;
+
+if(a>b && a>c)
+    {
+        console.log("A is Bigger.."+a);
+    }
+else if(b>c)
+    {
+        console.log("B is Bigger.."+b);
+    }
+else{
+        console.log("C is Bigger.."+c);
+}
+
+
+
+if(a<b && a<c)
+    {
+        console.log("A is Smaller.."+a);
+    }
+else if(b<c)
+    {
+        console.log("B is Smaller.."+b);
+    }
+else{
+        console.log("C is Smaller.."+c);
+}
+
+
+
+
+
---
+++ day-6/q2.js
@@ -0,0 +1,20 @@
+// Question 2: Determine the minimum and maximum of three given numbers.
+
+// Input: 7, 2, 9
+// Output: Minimum: 2, Maximum: 9
+
+let a=7;
+let b=2;
+let c=9;
+
+if(a>b)
+    {
+        if(a>c)
+            {
+                console.log("Maximum: "+a);
+            }
+        else
+            {
+                console.log("Maximum: "+c);
+            }
+    }
