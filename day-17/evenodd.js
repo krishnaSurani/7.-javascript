3cf8e99e291a04e433af2d3af3d83a21ffa7ab0d
suraniunnati <suraniunnati5@gmail.com>
2024-08-10 11:57:09 +0530

    day-17

---
+++ day-17/evenodd.js
@@ -0,0 +1,6 @@
+// Even Number ---
+
+for(let i=1; i<=20; i++)
+{
+    (i%2==0) ? console.log(i + " is a even number..") : console.log(i + " is a odd number..");
+}
---
+++ day-17/ternary.js
@@ -0,0 +1,18 @@
+// Maximum Number -----
+
+
+let a=48;
+let b=79;
+let c=16;
+
+(a>b && a>c) ? console.log(a + " is maximum number..") : (b>c) ? console.log(b + " is maximum number..") : console.log(c + " is maximum number.."); 
+
+
+// Minimum Number -----
+
+
+let d=48;
+let e=79;
+let f=16;
+
+(d<e && d<f) ? console.log(d + " is Minimum Number..") : (e<f) ? console.log(e + " is Minimum Number..") : console.log(f + " is Minimum Number.."); ;
